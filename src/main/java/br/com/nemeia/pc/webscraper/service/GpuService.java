61d9cf1e3417d31757a3546ed12ec2fd3d2d31ed
Ares <aresnemeia@gmail.com>
2024-12-07 09:13:07 -0300

    Set models back to this project

--- src/main/java/br/com/nemeia/pc/webscraper/service/GpuService.java
+++ src/main/java/br/com/nemeia/pc/webscraper/service/GpuService.java
@@ -1,11 +1,9 @@
 package br.com.nemeia.pc.webscraper.service;
 
-import br.com.nemeia.pc.webscraper.enums.Store;
 import br.com.nemeia.pc.webscraper.model.Gpu;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
